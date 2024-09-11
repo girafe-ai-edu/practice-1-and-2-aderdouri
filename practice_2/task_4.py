826b95682c831b2b7deeee77c6d91487bf205330
Abderrazak DERDOURI <abderrazak.derdouri@gmail.com>
2024-09-11 21:24:21 +0200

    task 4

--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Develop a program that asks the user for an integer 4-digit number and calculates the sum of its constituent digits. For example, if the user enters the number 3141, the program should output the following result:
-3 + 1 + 4 + 1 = 9
-
+Develop a program that asks the user for an integer 4-digit number 
+and calculates the sum of its constituent digits. 
+For example, if the user enters the number 3141, 
+the program should output the following result: 3 + 1 + 4 + 1 = 9
 """
+
+print("Enter an integer 4-digit number:")
+input_number = input()
+
+sum = 0
+for digit in str(input_number):
+    sum += int(digit)
+
+print("The sum of the constituent digits is: " + str(sum))
